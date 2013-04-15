a4f85053fce432679edb465a198ad3da8e517f9f
Yuhang Ge <abeyuhang@gmail.com>
2013-04-15 17:40:27 +0800

    xiaoge

--- test.js
+++ test.js
@@ -1,9 +1,15 @@
-var ffi = require('ffi');
+var FFI = require('ffi');
 
-var libm = ffi.Library('libm', {
-      'ceil': [ 'double', [ 'double' ] ]
+function TEXT(text){
+   return new Buffer(text, 'ucs2').toString('binary');
+}
+
+var user32 = new FFI.Library('user32', {
+   'MessageBoxW': [
+      'int32', [ 'int32', 'string', 'string', 'int32' ]
+   ]
 });
 
-var ans = libm.ceil(1.5);
-
-console.log(ans);
+var OK_or_Cancel = user32.MessageBoxW(
+   0, TEXT('I am Node. JS!\nI Love Daisy!'), TEXT('Hello, World!'), 1
+);
